80fdf21b0f70b6da6f3eb4c642aedea2d917cd35
Sperrotta10 <santiagoperrotta10@gmail.com>
2024-11-08 18:21:19 -0400

    New script for controller sound effects de buttons of menuMain

---
+++ RPGproyecto/Assets/Scripts/Menu/sound_buttons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound_buttons : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Obtén el componente AudioSource del botón
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        if (audioSource != null && !audioSource.enabled)
+        {
+            audioSource.enabled = true;
+        }
+        audioSource.Play();
+    }
+}
