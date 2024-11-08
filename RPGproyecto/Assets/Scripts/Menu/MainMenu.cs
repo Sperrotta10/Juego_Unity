3f88e005ece71bc78eb1f7dab6eadeb051fd0211
Sperrotta10 <santiagoperrotta10@gmail.com>
2024-11-08 18:15:08 -0400

    Add new implementation of menuMain script

--- RPGproyecto/Assets/Scripts/Menu/MainMenu.cs
+++ RPGproyecto/Assets/Scripts/Menu/MainMenu.cs
@@ -5,24 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void PlayGame()
     {
+        StartCoroutine(PlaySoundAndChangeScene());
+    }
+
+    private IEnumerator PlaySoundAndChangeScene()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+            yield return new WaitForSeconds(audioSource.clip.length); // Espera el tiempo de duración del sonido
+        }
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void GoToSettingsMenu()
-    {
-        SceneManager.LoadScene("SettingsMenu");
-    }
-
-    public void GoToMainMenu()
-
-    {
-        SceneManager.LoadScene("MainMenu");
     }
 
     public void QuitGame()
     {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+            Debug.Log("Salir del juego...");
+            StartCoroutine(QuitAfterSound());
+        }
+    }
+
+    private IEnumerator QuitAfterSound()
+    {
+        yield return new WaitForSeconds(audioSource.clip.length); // Espera a que el sonido termine
         Application.Quit();
     }
 }
