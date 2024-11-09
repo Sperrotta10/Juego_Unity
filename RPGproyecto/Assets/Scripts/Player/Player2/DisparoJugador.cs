b2547895385d3ba3850ae05d06858912c228dc5a
Rodrigo <rodrileogabriel@gmail.com>
2024-11-08 23:40:23 -0400

    Probando proyectiles en 18

---
+++ RPGproyecto/Assets/Scripts/Player/Player2/DisparoJugador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoJugador : MonoBehaviour
+{
+    [SerializeField] private Transform controladorDisparo;
+
+    [SerializeField] private GameObject bola;
+
+    private void Update(){
+        if(Input.GetButtonDown("attack2")){
+            //Disparar
+            Disparar();
+        }
+    }
+
+    private void Disparar()
+    {
+        Instantiate(bola, controladorDisparo.position, controladorDisparo.rotation);
+    }
+
+}
