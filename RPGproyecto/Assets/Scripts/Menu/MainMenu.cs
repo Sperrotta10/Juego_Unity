0195581b8792a14c1d9c5a085f0e39e075f22ce2
Sperrotta10 <santiagoperrotta10@gmail.com>
2024-11-09 21:07:45 -0400

    Modification script mainMenu

--- RPGproyecto/Assets/Scripts/Menu/MainMenu.cs
+++ RPGproyecto/Assets/Scripts/Menu/MainMenu.cs
@@ -6,42 +6,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    private AudioSource audioSource;
-
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
     public void PlayGame()
     {
-        StartCoroutine(PlaySoundAndChangeScene());
-    }
-
-    private IEnumerator PlaySoundAndChangeScene()
-    {
-        if (audioSource != null)
-        {
-            audioSource.Play();
-            yield return new WaitForSeconds(audioSource.clip.length); // Espera el tiempo de duración del sonido
-        }
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
-        if (audioSource != null)
-        {
-            audioSource.Play();
-            Debug.Log("Salir del juego...");
-            StartCoroutine(QuitAfterSound());
-        }
-    }
-
-    private IEnumerator QuitAfterSound()
-    {
-        yield return new WaitForSeconds(audioSource.clip.length); // Espera a que el sonido termine
+        Debug.Log("Salir del juego...");
         Application.Quit();
     }
 }
