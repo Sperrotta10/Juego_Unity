493ef3c2aaeda58d33531f29a61554a5bc7758ab
Rodrigo <rodrileogabriel@gmail.com>
2024-11-12 10:54:14 -0400

    Script and sprites for Freezer's prjectiles 2

---
+++ RPGproyecto/Assets/Scripts/RefJugadores/PlayerReferences.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerReferences", menuName = "ScriptableObjects/PlayerReferences", order = 1)]
+public class PlayerReferences : ScriptableObject
+{
+    public Transform player1; // Referencia al primer jugador
+    public Transform player2; // Referencia al segundo jugador
+}
