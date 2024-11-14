7c76f6ad707a3a59244d01787116048992424384
Sperrotta10 <santiagoperrotta10@gmail.com>
2024-11-14 13:43:14 -0400

    Modification script of menuMain

--- RPGproyecto/Assets/Scripts/Menu/MainMenu.cs
+++ RPGproyecto/Assets/Scripts/Menu/MainMenu.cs
@@ -6,11 +6,28 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public void PlayGame()
+    // Método que se llamará cuando se presione el botón "Easy"
+    public void OnEasyButtonPressed()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // Cargar la escena del nivel fácil
+        SceneManager.LoadScene("nivel_easy");
     }
 
+    // Método que se llamará cuando se presione el botón "Medium"
+    public void OnMediumButtonPressed()
+    {
+        // Cargar la escena del nivel medio
+        SceneManager.LoadScene("nivel_medium");
+    }
+
+    // Método que se llamará cuando se presione el botón "Hard"
+    public void OnHardButtonPressed()
+    {
+        // Cargar la escena del nivel difícil
+        SceneManager.LoadScene("nivel_hard");
+    }
+
+    // Método que se llamará cuando se presione el botón "salir"
     public void QuitGame()
     {
         Debug.Log("Salir del juego...");
